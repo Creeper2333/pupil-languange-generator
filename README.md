1d4314665ff269a6b8d776ceab7c36f3abd2762e
Creeper233 <52315359+Creeper2333@users.noreply.github.com>
2021-10-06 14:42:28 +0800

    updates

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
-# pupil-languange-generate
- 
+# 睿站小学生<sup>[[1]](#[1])</sup>发病模拟
+
+## 简介
+
+本项目旨在~~（比较）~~真实地还原睿站小学生无脑 ky，突发恶疾时的状态。本项目仅供娱乐，请不要上纲上线。
+
+## 使用方法
+
+### 配置词库
+
+本程序使用的词库结构如下：
+
+```json
+{
+    "bullshit":[wtf??],
+    "sentence_end_head":[text with brakes for the head of the content],
+    "sentence_end_body":[text with brakes for the body of the content],
+    "meme_sets":[
+        {
+            "set":[word1,word2],
+            "min_repeat": minimum repitition times for word 1,
+            "max_repeat": maximum repitition times for word 1,
+            "min_repeat_2": .......................... word 2,
+            "max_repeat_2": .......................... word 2
+        },
+        {
+            ...
+        }
+    			],
+   "meme_single":[
+            {
+            "value":word,
+            "min_repeat": minimum repitition times,
+            "max_repeat": maximum repitition times
+        },
+        {
+            ...
+        }
+            	],
+   "replaceable_head":[
+            {
+            "value": original, eg. 这个骷髅好像a,
+            replace:{
+            "(mark)": [option1, option2, ...] eg. "a":['sans','papyrus'], result: "这个骷髅好像sans", etc.
+        }
+        }
+            		]
+"replaceable_body":[same as "replaceable_head"]
+"content_end": end of the passage.
+}
+```
+
+理论上，通过自己修改词库，可以达到扩容或个性化的目的。（欢迎 pr 提交词库扩容！！(/≧▽≦)/  ）
+
+### 修改配置文件
+
+这里是一个程序使用的配置文件的示例：
+
+```ini
+[basics]
+word_list=nonsense.json
+
+[density]
+nanhun_density=0.1
+space_density=0.2
+sentence_end_density=0.1
+bullshit_density=0.05
+
+[content]
+body_paragraph=10
+head_control_length=500
+body_paragraph_control_length=5000
+use_ending=true
+
+[automation]
+generate_passage_numbers=2
+passage_auto_save=true
+bilibili_auto_ky=false
+```
+
+其中，word list 字段为使用的词库。
+
+nanhun density 字段则是“♂”符号出现的概率（越小概率越低，下同）。space density 是在句中打空格的概率，sentence end density指的是句末加括号文本的概率，bullshit density 是突发恶疾的概率。
+
+body paragraph 指正文段落数，head control length 为开头最大字符数（最好设的比较大），body paragraph control length 为正文最大字符数（同样设大点），use ending 则是选择是否要抽取一个结尾。
+
+generate passage numbers 指要生成几个文章，passage auto save 指是否自动保存生成的文章，bilibili auto ky 指是否要将生成的东西自动发到睿站（未做）。
+
+ps：这个文件已经在 repo 里了。
+
+### 运行
+
+配置好相关文件后，直接
+
+```
+> python main.py
+```
+
+即可。
+
+## 其它
+
+- 本程序遵守 MIT 开源协议。
+- 想法来源于 [menzi11/BullshitGenerator](https://github.com/menzi11/BullshitGenerator)
+
+## 注释
+
+##### <sup>[1]</sup>  这里的“小学生”不是传统意义上的小学生，剩下的应该不用我解释了。
---
+++ main.py
@@ -0,0 +1,176 @@
+import json
+from random import randint
+import configparser
+import time
+
+词库=None
+配置=None
+
+def read_config():
+    conf=configparser.ConfigParser()
+    path='config.ini'
+    conf.read(path)
+
+    基础=conf.items('basics')
+    词库文件=基础[0][1]
+
+    密度=conf.items('density')
+    男魂密度=1/float(密度[0][1])
+    空格密度=1/float(密度[1][1])
+    句末加括号密度=1/float(密度[2][1])
+    突发恶疾密度=1/float(密度[3][1])
+
+    文本设置=conf.items('content')
+    正文段落数=int(文本设置[0][1])
+    开头部分最大长度=int(文本设置[1][1])
+    正文段落最大长度=int(文本设置[2][1])
+    是否使用结尾段=str(文本设置[3][1])
+
+    迫真自动化=conf.items('automation')
+    生成文章数=int(迫真自动化[0][1])
+    自动保存文章=str(迫真自动化[1][1])
+    睿站ky胜者为王=str(迫真自动化[2][1])
+
+    return (
+        {
+            "word_file":词库文件,
+            "nanhun":男魂密度,
+            "space":空格密度,
+            "sentence_end":句末加括号密度,
+            "bullshit":突发恶疾密度,
+            "content_para":正文段落数,
+            "head_maxlen":开头部分最大长度,
+            "para_maxlen":正文段落最大长度,
+            "use_ending":是否使用结尾段,
+            "passages":生成文章数,
+            "autosave":自动保存文章,
+            "bili_ky":睿站ky胜者为王
+        }
+    )
+
+def 加载json(filename='nonsence.json'):
+    with open(filename,'r',encoding='utf-8') as file:
+        return json.loads(file.read())
+
+def 生成词汇组():
+    词汇组列表=词库['meme_sets']
+    词汇组=词汇组列表[randint(0,len(词汇组列表)-1)]
+    return (
+        词汇组['set'][0]*randint(
+            int(词汇组['min_repeat']),
+            int(词汇组['max_repeat'])
+        )+
+        词汇组['set'][1]*randint(
+            int(词汇组['min_repeat_2']),
+            int(词汇组['max_repeat_2'])
+        )
+    )
+
+def 生成词汇():
+    词汇列表=词库['meme_single']
+    词汇=词汇列表[randint(0,len(词汇列表)-1)]
+    return(
+        词汇['value']*randint(
+            词汇['min_repeat'],
+            词汇['max_repeat']
+        )
+    )
+
+def 生成可替换词汇_开头():
+    可替换=词库['replaceable_head'][randint(0,len(词库['replaceable_head'])-1)]
+    content=可替换['value']
+    for i in 可替换['replace']:
+        content=content.replace(i,
+            可替换['replace'][i][randint(0,len(可替换['replace'][i])-1)]
+        )
+    return content
+def 生成开头():
+    tmp=生成可替换词汇_开头()
+    end=词库['sentence_end_head']
+    while len(tmp)<=配置['head_maxlen']:
+        add=''
+        use_which=randint(0,100)
+        if use_which<10:
+            add=生成词汇组()
+        else:
+            add=生成词汇()
+        use_nanhun=randint(0,配置['nanhun'])
+        if use_nanhun==0 and ('♂' not in add):
+            nh=''
+            for i in add:
+                nh+=i.replace(i,i+'♂')
+            add=nh
+        else:
+            use_space=randint(0,配置['space'])
+            if use_space==0:
+                sp=''
+                for i in add:
+                    sp+=i.replace(i,i+' ')
+                add=sp.strip()
+            else:
+                use_end=randint(0,配置['sentence_end'])
+                if use_end==0:
+                    add+=end[randint(0,len(end)-1)]
+        tmp+=add
+        if randint(0,配置['bullshit'])==0:
+            tmp+=词库['bullshit'][randint(0,len(词库['bullshit'])-1)]*randint(1,3)
+        add=''
+    return tmp
+
+def 生成正文():
+    for i in range(0,配置['content_para']):
+        tmp=''
+        end=词库['sentence_end_body']
+        while len(tmp)<=配置['para_maxlen']:
+            add=''
+            use_which=randint(0,100)
+            if use_which<10:
+                add=生成词汇组()
+            else:
+                add=生成词汇()
+            use_nanhun=randint(0,配置['nanhun'])
+            if use_nanhun==0 and ('♂' not in add):
+                nh=''
+                for i in add:
+                    nh+=i.replace(i,i+'♂')
+                add=nh
+            else:
+                use_space=randint(0,配置['space'])
+                if use_space==0:
+                    sp=''
+                    for i in add:
+                        sp+=i.replace(i,i+' ')
+                    add=sp.strip()
+                else:
+                    use_end=randint(0,配置['sentence_end'])
+                    if use_end==0:
+                        add+=end[randint(0,len(end)-1)]
+            tmp+=add
+            if randint(0,配置['bullshit'])==0:
+                tmp+=词库['bullshit'][randint(0,len(词库['bullshit'])-1)]*randint(1,3)
+            add=''
+            #print(len(tmp))
+        tmp+='\n'
+    return tmp.strip()
+
+def 生成():
+    tmp=''
+    tmp+=生成开头()
+    tmp+=生成正文()
+    if 配置['use_ending'].lower()=='true':
+        tmp+=词库['content_end'][randint(0,len(词库['content_end'])-1)]
+    return tmp
+
+if __name__=='__main__':
+    配置=read_config()
+    词库=加载json(配置['word_file'])
+    passages=[]
+    for i in range(0,配置['passages']):
+        p=生成()
+        passages.append(p)
+        print(p)
+        if 配置['autosave']=='true':
+            with open('passage_'+time.strftime('%Y-%m-%d-%H-%M-%S')+' No.'+str(i+1)+'.txt','w',encoding='utf-8') as f:
+                f.write(p)
+                f.close()
+            
